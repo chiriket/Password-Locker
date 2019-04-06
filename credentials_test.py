f6330fe79649d695d4341fabb4583b04df13f9ee
Shirley Keter <shirleyketer@gmail.com>
2019-04-06 15:41:05 +0300

    added delete test

--- credentials_test.py
+++ credentials_test.py
@@ -44,10 +44,21 @@ class TestCredentials(unittest.TestCase):
             test_save_multiple_credentials to check if we can save multiple credentials
             objects to our credentials_list
             '''
-            self.new_credentials.save_credebtials()
-            test_credentials = Credentials("Account","pass") # new credentials
+            self.new_credentials.save_credentials()
+            test_credentials = Credentials("Account","Name","pass222") # new credentials
             test_credentials.save_credentials()
-            self.assertEqual(len(Credentials.credentials_list),2)
+            self.assertEqual(len(Credentials.credentials_list),3)
+
+    def test_delete_credentials(self):
+            '''
+            test_delete_credentials to test if we can remove a credentials from our credentials list
+            '''
+            self.new_credentials.save_credentials()
+            test_credentials = Credentials("Test","Account","name","pass222") # new credentials
+            test_credentials.save_credentials()
+
+            self.new_credentials.delete_credentials()# Deleting a credentials object
+            self.assertEqual(len(Credentials.credentials_list),1)
     
    
 if __name__ == '__main__':
