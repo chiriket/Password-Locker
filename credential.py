86232e6b0f876210352fb095ee384346f16451b9
Shirley Keter <shirleyketer@gmail.com>
2019-04-06 13:58:05 +0300

    added docstring

--- credential.py
+++ credential.py
@@ -1,11 +1,15 @@
 class Credentials:
-    """Create class for credentials"""
+    """
+    Class that generates new instances of credentials.
+    """ 
+
+    credentials_list = []
 
     def __init__(self,account_platform, account_name, account_password):
         self.account_platform = account_platform
         self.account_name = account_name
         self.account_password = account_password
 
-    credentials_list = []
+   
 
-    
+
