80930ea832b06cc8c65e11c5ad8e9bc133995d88
Shirley Keter <shirleyketer@gmail.com>
2019-04-06 12:32:26 +0300

    test for sve user

--- user_test.py
+++ user_test.py
@@ -26,12 +26,11 @@ class TestUser(unittest.TestCase):
     
     def test_save_user(self):
         '''
-        test_save_user test case to test if the contact object is saved into
+        test_save_user test case to test if the user object is saved into
          the user list
         '''
         self.new_user.save_user() # saving the new user
         self.assertEqual(len(User.user_list),1)
-
 
 
 if __name__ == '__main__':
