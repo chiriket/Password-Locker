602072539a91bfa3fff0472413e836b987712469
Shirley Keter <shirleyketer@gmail.com>
2019-04-06 13:50:56 +0300

    add credential_list

--- credential.py
+++ credential.py
@@ -1,8 +1,11 @@
 class Credentials:
     """Create class for credentials"""
 
-    def __init__(self, account_name, account_password):
+    def __init__(self,account_platform, account_name, account_password):
+        self.account_platform = account_platform
         self.account_name = account_name
         self.account_password = account_password
 
     credentials_list = []
+
+    
