b6747527102330a464d80e2b9f1deb2857dedbc8
Shirley Keter <shirleyketer@gmail.com>
2019-04-06 12:18:44 +0300

    add set upmethod and init test

--- user_test.py
+++ user_test.py
@@ -9,3 +9,21 @@ class TestUser(unittest.TestCase):
     Args:
         unittest.TestCase: TestCase class that helps in creating test cases
     '''
+
+    def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_user = Users("Chiri","pass123")
+
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+
+        self.assertEqual(self.new_user.login_name,("Chiri")
+        self.assertEqual(self.new_user.password,("pass123")
+
+
+if __name__ == '__main__':
+    unittest.main()
