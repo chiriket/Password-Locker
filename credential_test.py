48916cb0ebede0814355124d4dbf3b436b5565e1
Shirley Keter <shirleyketer@gmail.com>
2019-04-06 14:00:29 +0300

    imported unittest

---
+++ credential_test.py
@@ -0,0 +1,12 @@
+import unittest # Importing the unittest module
+from credential import Credential # Importing the contact class
+
+class TestCredential(unittest.TestCase):
+
+    '''
+    Test class that defines test cases for the credential class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
+
